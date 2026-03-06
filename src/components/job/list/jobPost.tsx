f96df8ab3dfd00a86cebdb2509ee3d8589f35915
Visava Chumnuam <visava.c@auroria.world>
2026-03-06 21:58:03 +0700

    fetch jobs list and detail

--- src/components/job/list/jobPost.tsx
+++ src/components/job/list/jobPost.tsx
@@ -3,14 +3,14 @@ import type { Job } from "../../../types/Job"
 const JobPost = ({ title, url, by, time } : Job) => {
 
     return (
-        <div className="grid grid-rows-2 gap-2 bg-white border-gray-100 border-2 rounded-sm p-4" role="listItem">
+        <div className=" bg-white border-gray-100 border-2 rounded-sm p-4" role="listItem">
 					<a 
 						className={ url ? "text-xl font-bold hover:underline" : "text-xl font-bold cursor-default"}
 						href={url}
 						target="_blank"
 						rel="noopener"
 					>{title}</a>
-					<span className="text-sm text-gray-600">
+					<span className="block mt-2 text-sm text-gray-600">
 						By {by} - {new Date(time * 1000).toLocaleString()}
 					</span>
         </div>
