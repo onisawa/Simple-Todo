1bd2b5b95d865b5cbe1a5da1910abbbdc667944c
Visava Chumnuam <visava.c@auroria.world>
2026-03-06 20:40:08 +0700

    Revert "initial job post list"
    This reverts commit 5f135bfe14298a9d32da4ff2ad33120ba3e76628.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import TodoList from "./components/todo/list"
 import { useTodos } from "./contexts/todo";
 import AddTodo from "./components/todo/add";
 import ConfirmModal from "./components/todo/common/confirmModal";
-import JobList from "./components/job/list";
 
 function App() {
   const {isError} = useTodos();
@@ -11,26 +10,19 @@ function App() {
   return (
     <>
      <section className='relative flex flex-col gap-y-2 mx-auto mt-10'>
-        {isError && (
-          <div className="absolute w-full h-full flex justify-center items-center bg-white/80 text-red-500">
-            <ExclamationCircleIcon className='size-6' />
-            <h1 className='px-1.5'>Something went wrong, Please try again.</h1>
+          {isError && (
+                <div className="absolute w-full h-full flex justify-center items-center bg-white/80 text-red-500">
+                  <ExclamationCircleIcon className='size-6' />
+                  <h1 className='px-1.5'>Something went wrong, Please try again.</h1>
+                </div>
+            )}
+          <div className='px-20'>
+            <h1 className='text-2xl font-bold mb-4'>Todo List</h1>
+            <AddTodo />
+            <TodoList />
+            <ConfirmModal />
           </div>
-        )}
-        <div className='px-20'>
-          <h1 className='text-2xl font-bold mb-4'>Todo List</h1>
-          <AddTodo />
-          <TodoList />
-          <ConfirmModal />
-        </div>
-      </section>      
-
-      <section className='relative flex flex-col gap-y-2 mx-auto mt-10'>
-        <div className='px-20'>
-          <h1 className='text-2xl font-bold mb-4'>Hacker News Job Board</h1>
-          <JobList />
-        </div>
-      </section>
+        </section>      
     </>
   )
 }
--- src/types/Job.ts
+++
@@ -1,6 +0,0 @@
-export interface Job {
-    title: string,
-    url: string,
-    by: string,
-    time: number
-}
