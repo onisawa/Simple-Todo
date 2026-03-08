cefe47ec81ea0e8f5c25ea02f7e7b94311cd1e91
Visava Chumnuam <visava.c@auroria.world>
2026-03-08 10:48:07 +0700

    styling nav bar

--- src/layouts/NavBar.tsx
+++ src/layouts/NavBar.tsx
@@ -3,11 +3,15 @@ import { Link, Outlet } from "react-router-dom"
 
 const NavBar = ({children}: PropsWithChildren) => {
   return (
-    <section>
-      <Link className="underline" to="/">[Back to Home]</Link>
+    <>
+    <section className="flex justify-end items-center pt-10 px-20 gap-2">
+      <Link className="underline" to="/">[Home]</Link>
+      <Link className="underline" to="/jobs">[Job Board]</Link>
+      <Link className="underline" to="/components">[Components]</Link>
+    </section>
 
       {children ?? <Outlet />}
-    </section>
+    </>
   )
 }
 
