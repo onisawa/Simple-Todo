157fcd5cd6883cc3b881f3f19f7b86fb2d4bf841
Visava Chumnuam <visava.c@auroria.world>
2026-03-09 21:41:57 +0700

    add responsive for job board

--- src/pages/JobBoard.tsx
+++ src/pages/JobBoard.tsx
@@ -3,7 +3,7 @@ import JobList from "@components/job/list";
 const JobBoardPage = () => {
   return (
       <section className='relative flex flex-col gap-y-2 mx-auto py-10 bg-amber-50'>
-        <div className='px-20'>
+        <div className='px-10 md:px-20'>
           <h1 className='text-2xl font-bold mb-4 text-orange-400'>Hacker News Job Board</h1>
           <JobList />
         </div>
