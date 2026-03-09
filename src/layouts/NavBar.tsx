757aa1de49c371c97f249ac48bf95ff349692e85
Visava Chumnuam <visava.c@auroria.world>
2026-03-09 08:32:30 +0700

    styling nav bar

--- src/layouts/NavBar.tsx
+++ src/layouts/NavBar.tsx
@@ -6,16 +6,18 @@ const components = ['accordian', 'progressbar', 'starRating'];
 const NavBar = ({children}: PropsWithChildren) => {
   return (
     <>
-    <section className="flex justify-end items-center pt-10 px-20 gap-2">
-      <NavLink className={({isActive}) => isActive ? 'font-bold' : 'underline'} to="/">[Home]</NavLink>
-      <NavLink className={({isActive}) => isActive ? 'font-bold' : 'underline'} to="/jobs">[Job Board]</NavLink>
-      <NavLink className={({isActive}) => isActive ? 'font-bold' : 'underline'} to="/components">[Components]</NavLink>
+    <section className="flex justify-end items-center py-5 px-20 gap-4 bg-gray-50 text-gray-400">
+      <NavLink className={({isActive}) => isActive ? 'main-menu-active' : 'main-menu'} to="/">Home</NavLink>
+      <hr className="vr" />
+      <NavLink className={({isActive}) => isActive ? 'main-menu-active' : 'main-menu'} to="/jobs">Job Board</NavLink>
+      <hr className="vr" />
+      <NavLink className={({isActive}) => isActive ? 'main-menu-active' : 'main-menu'} to="/components">Components</NavLink>
 
       <ul>
         {
           components.map((item, index) => (
             <li key={index}>
-              <NavLink className={({isActive}) => isActive ? 'font-bold' : 'underline'} to={`/components/${item}`}>[{item}]</NavLink>
+              <NavLink className={({isActive}) => isActive ? 'main-menu-active' : 'main-menu'} to={`/components/${item}`}>[{item}]</NavLink>
             </li>
           ))
         }
