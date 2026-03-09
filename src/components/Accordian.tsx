3aa2a76b89d0074c71bc7e7c50bbfc7a1632e150
Visava Chumnuam <visava.c@auroria.world>
2026-03-09 13:16:29 +0700

    add accordian

--- src/components/Accordian.tsx
+++ src/components/Accordian.tsx
@@ -6,36 +6,44 @@ const Accordian = () => {
   return (
     <>
       <h1 className='text-2xl font-bold mb-4 text-blue-400 uppercase'>Accordian</h1>
-      <button
-        onClick={() => setAccordionOpen(!accordionOpen)}
-        className="hover: cursor-pointer"
-      >
-        <svg
-          className="fill-indigo-500 shrink-0 ml-8"
-          width="16"
-          height="16"
-          xmlns="http://www.w3.org/2000/svg"
+      <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+        <button
+          onClick={() => setAccordionOpen(!accordionOpen)}
+          className="w-full flex justify-between items-center hover:cursor-pointer"
         >
-          <rect
-            y="7"
+          <span className="font-bold text-xl">Question ?</span>
+          <svg
+            className="fill-indigo-500 shrink-0 ml-8"
             width="16"
-            height="2"
-            rx="1"
-            className={`transform origin-center transition duration-200 ease-out ${
-              accordionOpen && "rotate-180!"
-            }`}
-          />
-          <rect
-            y="7"
-            width="16"
-            height="2"
-            rx="1"
-            className={`transform origin-center rotate-90 transition duration-200 ease-out ${
-              accordionOpen && "rotate-180!"
-            }`}
-          />
-        </svg>
-      </button>
+            height="16"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              y="7"
+              width="16"
+              height="2"
+              rx="1"
+              className={`transform origin-center transition duration-200 ease-out ${
+                accordionOpen && "rotate-180!"
+              }`}
+            />
+            <rect
+              y="7"
+              width="16"
+              height="2"
+              rx="1"
+              className={`transform origin-center rotate-90 transition duration-200 ease-out ${
+                accordionOpen && "rotate-180!"
+              }`}
+            />
+          </svg>
+        </button>
+        <div className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${
+          accordionOpen ? 'p-2 grid-rows-[1fr] opacity: 100%' : 'grid-rows-[0fr] opacity: 0%'
+        }`}>
+          <span className="overflow-hidden">Answer</span>
+        </div>
+      </div>
     </>
 
   )
