00eda0ee1f52dd3bf04fe3a596e0dcfba08567ff
Visava Chumnuam <visava.c@auroria.world>
2026-03-10 15:59:17 +0700

    add color parameters

--- src/components/NotificationToggle.tsx
+++ src/components/NotificationToggle.tsx
@@ -1,18 +1,18 @@
 import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
 
-const NotificationToggle = () => {
+const NotificationToggle = ({enabledColor, disabledColor} : {enabledColor: string, disabledColor: string}) => {
   const [isActive, setIsActive] = useState(false);
 
   return (
     <>
-      <label className={`cursor-pointer ${isActive ? 'bg-green-400' : 'bg-red-400'} w-20 h-10 rounded-4xl flex items-center`}>
+      <label className={`cursor-pointer w-20 h-10 rounded-4xl flex items-center`} style={{ backgroundColor: isActive ? enabledColor : disabledColor }}>
         <input type="checkbox" className="sr-only peer" onClick={() => setIsActive(!isActive)} />
         <span className="bg-white w-2/5 h-4/5 rounded-4xl ml-1 peer-checked:ml-11 transition-all duration-300 ease-in-out" />
       </label>
       <div className="flex">
         <div>
-          { isActive ? <CheckCircleIcon className="size-6 text-white fill-green-400" /> : <XCircleIcon className="size-6 text-white fill-red-600" /> }
+          { isActive ? <CheckCircleIcon className={`size-6 text-white`} style={{ fill: enabledColor }} /> : <XCircleIcon className={`size-6 text-white bg-[${disabledColor}]`} style={{ fill: disabledColor }} /> }
         </div>
         Notifications are {isActive ? 'enabled' : 'disabled'}
       </div>
--- src/pages/NotificationPage.tsx
+++ src/pages/NotificationPage.tsx
@@ -4,7 +4,8 @@ const NotificationPage = () => {
   return (
     <>
       <h1 className='text-2xl font-bold my-4 text-blue-400 uppercase'>Notification Toggle</h1>
-      <NotificationToggle />
+      <NotificationToggle enabledColor="#008000" disabledColor="#EF5350" />
+      <NotificationToggle enabledColor="#008000" disabledColor="#EEEEEE" />
     </>
   )
 }
